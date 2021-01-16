e87d9f0820337beaf26db997009c986a23559b2a
cqueiro2 <cqueiro2@hotmail.com>
2021-01-15 23:45:09 -0300

    The Sigle page Biorhythms

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,45 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonDatetime,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React, { useState } from "react";
+
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const [name, setName] = useState('')
+  const [birthDate, setBirthDate] = useState<string>('')
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Biorhythms</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonLabel position="stacked">Name</IonLabel>
+          <IonInput value={name} 
+          onIonChange={(event: any) => setName(event.detail.value)}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Date of Birth</IonLabel>
+          <IonDatetime 
+          displayFormat="D-MMM-YYYY"
+          value={birthDate}
+           onIonChange={event => setBirthDate(event.detail.value!)}
+          />
+        </IonItem>
+        <p>Name: {name}</p>
+        <p>Date of Birth: {birthDate}</p>
       </IonContent>
     </IonPage>
   );
